ff839b68d9c7ad90f0d7734bb5e33991fe3605ab
nabeff <neffina8@gmail.com>
2025-09-02 13:36:16 +0100

    email env chnaged email v2

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -105,7 +105,7 @@ export default buildConfig({
     tasks: [],
   },
   email: nodemailerAdapter({
-    defaultFromAddress: 'contact@afri-sol.com',
+    defaultFromAddress: 'nabileffina@webloo.com',
     defaultFromName: 'Afrisol',
     transport: nodemailer.createTransport({
       service: 'Gmail',
