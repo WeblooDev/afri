7c69a09d7f6802bb52c2b6186207dd1b3b533ae4
nabeff <neffina8@gmail.com>
2025-09-02 14:02:16 +0100

    email env chnaged email v6

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -44,6 +44,7 @@ export const HeaderClient: React.FC<{ data: Header }> = ({ data }) => {
               <Media resource={data.logo} priority className="h-10 !w-auto object-contain" />
             )}
           </Link>
+          <p>pe</p>
         </div>
 
         {/* Desktop Nav */}
